1a394646c04b559659fe5b1f181c7acc196f9460
Raushan Raj <kumarraushanraj10@gmail.com>
2025-05-11 18:54:09 +0530

    dyanmic data posted:

--- src/app.js
+++ src/app.js
@@ -4,17 +4,30 @@ const connectDB = require("./config/Database.js")
 const User=require("./models/user.js")
 const app = express();
 
+app.use(express.json());
+
+
 app.post("/singup", async (req,res)=>{
-    const userobj={
-        firstName:"rani",
-        lastName:"Raj",
-        age:25,
-        Email:"rani@gmail.com",
-        password:"1234"
+    
+    // const userobj={
+    //     firstName:"rani",
+    //     lastName:"Raj",
+    //     age:25,
+    //     Email:"rani@gmail.com",
+    //     password:"1234"
+    // }
+    // const user=new User(userobj);
+    // await user.save();
+
+
+    const user=new User(req.body)
+
+    try{
+        await user.save()
+        res.send("data saved")
+    }catch(err){
+        res.status(400).send("error,not able to save")
     }
-    const user=new User(userobj);
-    await user.save();
-    res.send("data save successfully")
 })
 
 
